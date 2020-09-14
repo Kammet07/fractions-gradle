4df8261e708f5876fb64cf58b8666a64bd87dd05
Kammet07 <pavel.pupentsov@educanet.cz>
2020-09-14 18:24:57 +0200

    +-

--- src/main/java/Fraction.java
+++ src/main/java/Fraction.java
@@ -22,12 +22,20 @@ public class Fraction implements IFraction {
 
     @Override
     public IFraction plus(IFraction other) {
-        throw new UnsupportedOperationException();
+        int lowestCommonMultiple = findLowestCommonMultiple(getDenominator(), other.getDenominator());
+        int numerator1 = lowestCommonMultiple / denominator * numerator;
+        int numerator2 = lowestCommonMultiple / other.getDenominator() * other.getNumerator();
+
+        return new Fraction(numerator1 + numerator2, lowestCommonMultiple);
     }
 
     @Override
     public IFraction minus(IFraction other) {
-        throw new UnsupportedOperationException();
+        int lowestCommonMultiple = findLowestCommonMultiple(getDenominator(), other.getDenominator());
+        int numerator1 = lowestCommonMultiple / denominator * numerator;
+        int numerator2 = lowestCommonMultiple / other.getDenominator() * other.getNumerator();
+
+        return new Fraction(numerator1 - numerator2, lowestCommonMultiple);
     }
 
     @Override
@@ -47,5 +55,28 @@ public class Fraction implements IFraction {
     @Override
     public String toString() {
         return "Fraction " + numerator + "|" + denominator;
+    }
+
+    private static Integer findGreatestCommonDenominator(Integer i1, Integer i2) {
+        if (i1 < i2) return findGreatestCommonDenominator(i2, i1);
+        if (i2 == 0) return i1;
+        return findGreatestCommonDenominator(i2, i1 % i2);
+    }
+
+    private static Integer findLowestCommonMultiple(Integer i1, Integer i2) {
+        if (i1 == 0 || i2 == 0) return 0;
+        else {
+            int gcd = findGreatestCommonDenominator(i1, i2);
+            return Math.abs(i1 * i2) / gcd;
+        }
+    }
+
+    public static void main(String[] args) {
+        Fraction fraction1 = new Fraction(2, 3);
+        Fraction fraction2 = new Fraction(3, 2);
+
+        System.out.println(fraction1.plus(fraction2).getNumerator() + "|" + fraction1.plus(fraction2).getDenominator());
+        System.out.println(fraction1.minus(fraction2).getNumerator() + "|" + fraction1.minus(fraction2).getDenominator());
+
     }
 }
